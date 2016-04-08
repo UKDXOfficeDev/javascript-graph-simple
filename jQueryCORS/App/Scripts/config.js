b7d5736b97bc477f221ff8c80c33cf73c4dec68a
Pete Daukintis <peted70@hotmail.com>
2016-04-08 15:58:11 +0100

    added

---
+++ jQueryCORS/App/Scripts/config.js
@@ -0,0 +1,5 @@
+﻿// If your account is 'yourdomain.onmicrosoft.com', this value should be 'yourdomain'.
+var tenant = 'fitnesscloud';
+
+// Get this value from the Configure tab of your application page in Azure Management Portal.
+var clientId = '3ae50032-7bd6-4955-abb5-b1f896894e83';
